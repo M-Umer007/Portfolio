c5f3414a507868953c746d3ef655546a44cd895b
M.Umer <umeraijaz32@gmail.com>
2024-11-10 05:34:52 -0800

    remove some errors

--- src/app/allProjects/components/Button/page.tsx
+++ src/app/allProjects/components/Button/page.tsx
@@ -1,11 +1,7 @@
-
-
-
-
-export default function Button({    children   }    : {     children: React.ReactNode    }) {
+export default function Button({    children}: {children: any   }) {
     return (
       <div className="inline-flex  items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
-{children}
+  {children}
               </div>
 );}
   
